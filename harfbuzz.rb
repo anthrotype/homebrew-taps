e9d6e647073a2bdf4f8ca7eeabaacc98d8f9be5d
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-07-22 12:45:41 +0100

    [harfbuzz] make --with-coretext optional

--- harfbuzz.rb
+++ harfbuzz.rb
@@ -27,6 +27,8 @@ class Harfbuzz < Formula
   depends_on "freetype"
   depends_on "gobject-introspection"
 
+  option "with-coretext", "Compile with CoreText bindings"
+
   resource "ttf" do
     url "https://github.com/behdad/harfbuzz/raw/fc0daafab0336b847ac14682e581a8838f36a0bf/test/shaping/fonts/sha1sum/270b89df543a7e48e206a2d830c0e10e5265c630.ttf"
     sha256 "9535d35dab9e002963eef56757c46881f6b3d3b27db24eefcc80929781856c77"
@@ -42,7 +44,6 @@ class Harfbuzz < Formula
       --prefix=#{prefix}
       --enable-introspection=yes
       --with-gobject=yes
-      --with-coretext=yes
     ]
 
     make_args = %W[]
@@ -54,6 +55,7 @@ class Harfbuzz < Formula
 
     configure_args << "--with-icu" if build.with? "icu4c"
     configure_args << "--with-graphite2" if build.with? "graphite2"
+    configure_args << "--with-coretext" if build.with? "coretext"
     system "./configure", *configure_args
     system "make", "install", *make_args
   end
